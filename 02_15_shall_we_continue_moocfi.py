37af3ae912100420a576cf33a3979395b9ebc7d0
Thomas Tannenberg <114718959+ThomasTannenberg@users.noreply.github.com>
2023-07-08 07:49:57 +0200

    Add files via upload

---
+++ 02_15_shall_we_continue_moocfi.py
@@ -0,0 +1,10 @@
+def check():
+    while True:
+        print("hi")
+        user_input = input("Shall we continue? ")
+        if user_input == "no":
+            print("okay then")
+            break
+
+check()
+
