fe272a8fcfdcd1ff27ed62605711c6031f34c1ed
Thomas Tannenberg <114718959+ThomasTannenberg@users.noreply.github.com>
2023-07-13 18:14:00 +0200

    Add files via upload

---
+++ 04_03_box_of_hashes_moocfi.py
@@ -0,0 +1,16 @@
+# Copy here code of line function from previous exercise
+def line(number, string):
+
+    if string == "":
+        print(number * "*")
+    else:
+        print(number * string[0])
+
+def box_of_hashes(height):
+    # You should call function line here with proper parameters
+    for i in range(1, height + 1):
+        line(10, "#")
+
+# You can test your function by calling it within the following block
+if __name__ == "__main__":
+    box_of_hashes(5)
