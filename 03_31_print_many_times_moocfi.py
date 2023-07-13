dd2332cc98d88f66f82a0c0b7348623548678850
Thomas Tannenberg <114718959+ThomasTannenberg@users.noreply.github.com>
2023-07-13 17:06:00 +0200

    Add files via upload

---
+++ 03_31_print_many_times_moocfi.py
@@ -0,0 +1,8 @@
+# Write your solution here
+def print_many_times(text, number):
+    for i in range (1, number + 1):
+        print(text)
+
+# You can test your function by calling it within the following block
+if __name__ == "__main__":
+    print_many_times("python", 5)
