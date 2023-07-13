95bdeefc17aaa2c861c37959180e8ac7a147b979
Thomas Tannenberg <114718959+ThomasTannenberg@users.noreply.github.com>
2023-07-13 17:09:03 +0200

    Add files via upload

---
+++ 03_32_square_of_hashes_moocfi.py
@@ -0,0 +1,7 @@
+# Write your solution here
+def hash_square(length):
+    for i in range (1, length + 1):
+        print(length * '#')
+# You can test your function by calling it within the following block
+if __name__ == "__main__":
+    hash_square(5)
