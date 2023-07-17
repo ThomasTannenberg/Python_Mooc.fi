57a0d6992e8e4b2add7bbb2cd93771287a90d7ca
Thomas Tannenberg <114718959+ThomasTannenberg@users.noreply.github.com>
2023-07-17 12:50:11 +0200

    Add files via upload

---
+++ 04_12_change_value_of_item_moocfi.py
@@ -0,0 +1,9 @@
+list = [1, 2, 3, 4, 5]
+
+while True:
+    index = int(input("Index: "))
+    if index == -1:
+        break
+    value = int(input("New value: "))
+    list[index] = value
+    print(list)
