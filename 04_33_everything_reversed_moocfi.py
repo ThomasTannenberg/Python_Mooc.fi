8f17b8efe5b747a78e86f5ac1c5cbb565f5bf4ca
Thomas Tannenberg <114718959+ThomasTannenberg@users.noreply.github.com>
2023-08-16 15:01:07 +0200

    Add files via upload

---
+++ 04_33_everything_reversed_moocfi.py
@@ -0,0 +1,15 @@
+# Write your solution here
+def everything_reversed(list):
+    new_list = []
+    for word in list:
+        new_list.append(word[::-1])
+
+
+
+    return new_list[::-1]
+
+if __name__ == "__main__":
+    my_list = ["Hi", "there", "example", "one more"]
+    new_list = everything_reversed(my_list)
+    print(new_list)
+
