318086cff5dffc4f8af079ba8874baf757f990f6
Thomas Tannenberg <114718959+ThomasTannenberg@users.noreply.github.com>
2023-08-22 14:30:15 +0200

    Add files via upload

---
+++ 05_12_tic_tac_toe_moocfi.py
@@ -0,0 +1,15 @@
+# Write your solution here
+
+def play_turn(game_board: list, x: int, y: int, piece: str) -> bool:
+    # Check for valid coordinates
+    if 0 <= x <= 2 and 0 <= y <= 2:
+        # Check for an empty square
+        if game_board[y][x] == "":
+            game_board[y][x] = piece
+            return True
+    return False
+
+if __name__ == '__main__':
+    game_board = [["", "", ""], ["", "", ""], ["", "", ""]]
+    print(play_turn(game_board, 2, 0, "X"))
+    print(game_board)
