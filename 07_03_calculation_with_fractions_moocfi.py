3d8f33f3a0ee6894c97de89aba4ceae8a6ea3a04
Thomas Tannenberg <114718959+ThomasTannenberg@users.noreply.github.com>
2023-09-11 13:34:57 +0200

    Add files via upload

---
+++ 07_03_calculation_with_fractions_moocfi.py
@@ -0,0 +1,19 @@
+from fractions import Fraction
+
+def fractionate(amount: int):
+    fraction_list = []
+
+    for i in range(amount):
+        fraction_list.append(Fraction(1, amount))
+    
+    return fraction_list
+
+
+
+if __name__ == '__main__':
+    for p in fractionate(3):
+        print(p)
+
+    print()
+
+    print(fractionate(5))
